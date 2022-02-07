443f266c1065f928ce43cec50bba18bc293c779e
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-02-07 15:04:57 -0800

    Create PlayerStatistics class for EFCore
    closes #4

---
+++ BoredWithFriends/Models/PlayerStatistics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Models
+{
+	internal class PlayerStatistics
+	{
+		[Key]
+		public int PlayerID { get; set; }
+
+		public DateTime LastPlayedTime { get; set; }
+
+		public long TotalPlayTime { get; set; } //in ms.
+
+		public int RoundsPlayed { get; set; }
+
+		public int Wins { get; set; }
+
+		public int Losses { get; set; }
+
+	}
+}
