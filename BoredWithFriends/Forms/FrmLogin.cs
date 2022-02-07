2cfd9a75ee82268e0ffe4e3b1480aded112868b1
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-02-07 13:21:59 -0800

    Refactored/Organized Forms and their namespaces
    closes #17
    I also ran code cleanup (which should enforce the editor config).

---
+++ BoredWithFriends/Forms/FrmLogin.cs
@@ -0,0 +1,15 @@
+﻿namespace BoredWithFriends.Forms
+{
+	public partial class FrmLogin : Form
+	{
+		public FrmLogin()
+		{
+			InitializeComponent();
+		}
+
+		private void btnBackToInitialLauncher_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
+}
