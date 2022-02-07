b178bfd4a17a2a099921f3902f0a1ed95781432a
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-02-07 15:04:26 -0800

    Create PlayerLogin class for EFCore
    closes #3

---
+++ BoredWithFriends/Models/PlayerLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Models
+{
+	internal class PlayerLogin
+	{
+		[Key]
+		public int PlayerID { get; private set; }
+
+		public string UserName { get; set; } = null!;
+
+		public string Password { get; set; } = null!;
+
+		public DateTime LastLoginTime { get; private set; }
+
+	}
+}
