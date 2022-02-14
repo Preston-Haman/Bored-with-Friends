c54243b2c003fc76352970682570052235670100
Kris <dauthi66@gmail.com>
2022-02-13 17:51:43 -0800

    Created account creation form, initialized two buttons.

---
+++ BoredWithFriends/Forms/FrmAccountCreation.cs
@@ -0,0 +1,31 @@
+﻿using BoredWithFriends.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoredWithFriends.Forms
+{
+	public partial class FrmAccountCreation : Form
+	{
+		public FrmAccountCreation()
+		{
+			InitializeComponent();
+		}
+
+		private void btnBackToLogin_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void btnCreateAccount_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
