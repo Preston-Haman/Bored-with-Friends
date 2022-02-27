d44e7d28e8151ae912a76d70423949470c8b56fd
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-02-26 23:23:43 -0800

    Created CtrlMatchFourBoard Control class
    This class is a visual representation of the Match Four game board. It does not allow the user to play the game directly.

---
+++ BoredWithFriends/Controls/CtrlMatchFourBoard.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace BoredWithFriends.Controls
+{
+	partial class CtrlMatchFourBoard
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// CtrlMatchFour
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Yellow;
+			this.Name = "CtrlMatchFour";
+			this.Size = new System.Drawing.Size(469, 402);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.CtrlMatchFour_Paint);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
