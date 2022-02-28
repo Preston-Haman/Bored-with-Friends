bdc7d65f5952d47829b083bc77c3970a6b559367
Kris <dauthi66@gmail.com>
2022-02-27 17:19:22 -0800

    Created visual layout

--- BoredWithFriends/Forms/FrmInitialLauncher.Designer.cs
+++ BoredWithFriends/Forms/FrmInitialLauncher.Designer.cs
@@ -28,67 +28,202 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.btnLocalGameLaunch = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.btnMultiplayerGameLaunch = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// btnLocalGameLaunch
-			// 
-			this.btnLocalGameLaunch.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.btnLocalGameLaunch.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-			this.btnLocalGameLaunch.Location = new System.Drawing.Point(57, 101);
-			this.btnLocalGameLaunch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-			this.btnLocalGameLaunch.Name = "btnLocalGameLaunch";
-			this.btnLocalGameLaunch.Size = new System.Drawing.Size(153, 78);
-			this.btnLocalGameLaunch.TabIndex = 0;
-			this.btnLocalGameLaunch.Text = "Local Play\r\n(Hot Seat)\r\n";
-			this.btnLocalGameLaunch.UseVisualStyleBackColor = false;
-			this.btnLocalGameLaunch.Click += new System.EventHandler(this.btnLocalGameLaunch_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Sigmar One", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(395, 53);
-			this.label1.TabIndex = 2;
-			this.label1.Text = "Bored with Friends";
-			// 
-			// btnMultiplayerGameLaunch
-			// 
-			this.btnMultiplayerGameLaunch.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.btnMultiplayerGameLaunch.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-			this.btnMultiplayerGameLaunch.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-			this.btnMultiplayerGameLaunch.Location = new System.Drawing.Point(275, 101);
-			this.btnMultiplayerGameLaunch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-			this.btnMultiplayerGameLaunch.Name = "btnMultiplayerGameLaunch";
-			this.btnMultiplayerGameLaunch.Size = new System.Drawing.Size(153, 78);
-			this.btnMultiplayerGameLaunch.TabIndex = 3;
-			this.btnMultiplayerGameLaunch.Text = "Online\r\nMultiplayer\r\n";
-			this.btnMultiplayerGameLaunch.UseVisualStyleBackColor = false;
-			// 
-			// FrmInitialLauncher
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 211);
-			this.Controls.Add(this.btnMultiplayerGameLaunch);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.btnLocalGameLaunch);
-			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-			this.Name = "FrmInitialLauncher";
-			this.Text = "Launcher";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.cbxPlayOnline = new System.Windows.Forms.CheckBox();
+            this.lblServerIP = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblPlayOffline = new System.Windows.Forms.Label();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtServerIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnGuest = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Sigmar One", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(20, 1);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(495, 66);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Bored with Friends";
+            // 
+            // cbxPlayOnline
+            // 
+            this.cbxPlayOnline.AutoSize = true;
+            this.cbxPlayOnline.Font = new System.Drawing.Font("Sigmar One", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbxPlayOnline.Location = new System.Drawing.Point(38, 86);
+            this.cbxPlayOnline.Name = "cbxPlayOnline";
+            this.cbxPlayOnline.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cbxPlayOnline.Size = new System.Drawing.Size(157, 32);
+            this.cbxPlayOnline.TabIndex = 3;
+            this.cbxPlayOnline.Text = "Play Online";
+            this.cbxPlayOnline.UseVisualStyleBackColor = true;
+            // 
+            // lblServerIP
+            // 
+            this.lblServerIP.AutoSize = true;
+            this.lblServerIP.Font = new System.Drawing.Font("Sigmar One", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblServerIP.Location = new System.Drawing.Point(43, 130);
+            this.lblServerIP.Name = "lblServerIP";
+            this.lblServerIP.Size = new System.Drawing.Size(111, 28);
+            this.lblServerIP.TabIndex = 4;
+            this.lblServerIP.Text = "Server IP";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("Sigmar One", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPort.Location = new System.Drawing.Point(341, 130);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(63, 28);
+            this.lblPort.TabIndex = 5;
+            this.lblPort.Text = "Port";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Sigmar One", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUsername.Location = new System.Drawing.Point(42, 189);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(115, 28);
+            this.lblUsername.TabIndex = 6;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Sigmar One", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPassword.Location = new System.Drawing.Point(43, 222);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(118, 28);
+            this.lblPassword.TabIndex = 7;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblPlayOffline
+            // 
+            this.lblPlayOffline.AutoSize = true;
+            this.lblPlayOffline.Font = new System.Drawing.Font("Sigmar One", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPlayOffline.Location = new System.Drawing.Point(80, 313);
+            this.lblPlayOffline.Name = "lblPlayOffline";
+            this.lblPlayOffline.Size = new System.Drawing.Size(279, 56);
+            this.lblPlayOffline.TabIndex = 8;
+            this.lblPlayOffline.Text = "OR\r\nContinue Offline as Guest";
+            this.lblPlayOffline.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(43, 265);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(138, 29);
+            this.btnCreateAccount.TabIndex = 9;
+            this.btnCreateAccount.Text = "Create Account";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(265, 265);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(94, 29);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtServerIP
+            // 
+            this.txtServerIP.Location = new System.Drawing.Point(158, 129);
+            this.txtServerIP.Name = "txtServerIP";
+            this.txtServerIP.Size = new System.Drawing.Size(158, 27);
+            this.txtServerIP.TabIndex = 11;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(410, 131);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(94, 27);
+            this.txtPort.TabIndex = 12;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(158, 190);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(201, 27);
+            this.txtUsername.TabIndex = 13;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(158, 223);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(201, 27);
+            this.txtPassword.TabIndex = 14;
+            // 
+            // btnGuest
+            // 
+            this.btnGuest.Location = new System.Drawing.Point(376, 340);
+            this.btnGuest.Name = "btnGuest";
+            this.btnGuest.Size = new System.Drawing.Size(104, 29);
+            this.btnGuest.TabIndex = 15;
+            this.btnGuest.Text = "Play as Guest";
+            this.btnGuest.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 386);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(62, 29);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // FrmInitialLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 427);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGuest);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtServerIP);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.lblPlayOffline);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblServerIP);
+            this.Controls.Add(this.cbxPlayOnline);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "FrmInitialLauncher";
+            this.Text = "Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
         #endregion
-
-        private Button btnLocalGameLaunch;
-        private Label label1;
-        private Button btnMultiplayerGameLaunch;
-    }
+        private Label lblTitle;
+		private CheckBox cbxPlayOnline;
+		private Label lblServerIP;
+		private Label lblPort;
+		private Label lblUsername;
+		private Label lblPassword;
+		private Label lblPlayOffline;
+		private Button btnCreateAccount;
+		private Button btnLogin;
+		private TextBox txtServerIP;
+		private TextBox txtPort;
+		private TextBox txtUsername;
+		private TextBox txtPassword;
+		private Button btnGuest;
+		private Button btnExit;
+	}
 }
