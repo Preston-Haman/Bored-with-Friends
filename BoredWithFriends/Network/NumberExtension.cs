fd74205a607cca97f3f7005b3ce7eacc98901a27
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-05 02:57:31 -0800

    Created NumberExtension class
    This class is meant for helper methods for numeric values. Currently, I've implemented simple methods to retrieve the byte at a given position in a larger value.

---
+++ BoredWithFriends/Network/NumberExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Network
+{
+	internal static class NumberExtension
+	{
+		public static byte GetByteAtPosition(this short value, int position)
+		{
+			return (byte) ((value >> position * 8) & 0xFF);
+		}
+
+		public static byte GetByteAtPosition(this int value, int position)
+		{
+			return (byte) ((value >> position * 8) & 0xFF);
+		}
+
+		public static byte GetByteAtPosition(this long value, int position)
+		{
+			return (byte) ((value >> position * 8) & ((long) 0xFF));
+		}
+	}
+}
