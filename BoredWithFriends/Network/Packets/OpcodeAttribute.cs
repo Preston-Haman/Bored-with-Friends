3d1ba34d2cd1bda667bbf1c947f2a7a90cbcd92d
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-05 08:38:36 -0800

    Created Custom Attributes for Packets
    These attributes will allow marking packet implementations with their protocol, and opcode; they will also register packet implementations into a dictionary so the implementations can be identified by the raw packet headers later.

---
+++ BoredWithFriends/Network/Packets/OpcodeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Network.Packets
+{
+	[AttributeUsage(AttributeTargets.Class)]
+	internal class OpcodeAttribute : Attribute
+	{
+		public short Opcode { get; }
+
+		public OpcodeAttribute(short opCode)
+		{
+			Opcode = opCode;
+		}
+	}
+}
