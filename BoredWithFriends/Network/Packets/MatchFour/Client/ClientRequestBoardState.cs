63e72a37d88381d577e77c711cc016f3aa3b06c9
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-05 18:18:06 -0800

    Added Initial Client/Server Packets for Match Four
    These packets aren't implemented fully yet.

---
+++ BoredWithFriends/Network/Packets/MatchFour/Client/ClientRequestBoardState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Network.Packets.MatchFour.Client
+{
+	[Packet(typeof(ClientRequestBoardState))]
+	[Protocol((short) BoredWithFriendsProtocol.MatchFour)]
+	[Opcode((short) MatchFourOps.RequestBoardState)]
+	internal class ClientRequestBoardState : ClientPacket
+	{
+		public ClientRequestBoardState()
+		{
+			//For reflection
+		}
+
+		protected override void ReadImpl()
+		{
+			//Nothing to do.
+		}
+
+		protected override void RunImpl()
+		{
+			//TODO: Reply with SendBoardState
+			throw new NotImplementedException();
+		}
+
+		protected override void WriteImpl()
+		{
+			//Nothing to do.
+		}
+	}
+}
