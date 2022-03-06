951b958dc50fcf5338241bb4a66611f5770142a4
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-05 18:18:06 -0800

    Added Initial Client/Server Packets for Match Four
    These packets aren't implemented fully yet.

---
+++ BoredWithFriends/Network/Packets/MatchFour/Client/ClientClearBoard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Network.Packets.MatchFour.Client
+{
+	[Packet(typeof(ClientClearBoard))]
+	[Protocol((short) BoredWithFriendsProtocol.MatchFour)]
+	[Opcode((short) MatchFourOps.ClearBoard)]
+	internal class ClientClearBoard : ClientPacket
+	{
+		public ClientClearBoard()
+		{
+			//For reflection
+		}
+
+		protected override void ReadImpl()
+		{
+			//Nothing to do.
+		}
+
+		protected override void RunImpl()
+		{
+			//TODO: Get gamestate for this packet and forfeit this player; reply with BoardCleared
+			throw new NotImplementedException();
+		}
+
+		protected override void WriteImpl()
+		{
+			//Nothing to do.
+		}
+	}
+}
---
+++ BoredWithFriends/Network/Packets/MatchFour/Server/ServerSendBoardState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Network.Packets.MatchFour.Server
+{
+	internal class ServerSendBoardState : ServerPacket
+	{
+		protected override void ReadImpl()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void RunImpl()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void WriteImpl()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
