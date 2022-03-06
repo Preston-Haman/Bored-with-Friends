d4eefd9865d7d3930a855a39fc291a67375d79c9
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-05 18:15:34 -0800

    Added Protocol & Opcode Enumerations
    These are for packet implementations so they can specify their protocol and opcode in a way that prevents accidental overlap.

---
+++ BoredWithFriends/Network/Packets/General/GeneralOps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Network.Packets.General
+{
+	internal enum GeneralOps
+	{
+		//Client Ops
+		Connect,
+		CreateAccount,
+		Login,
+		AccountManagement,
+		UpdatePassword,
+		DeleteAccount,
+		SelectGame,
+		Disconnect,
+
+		//Server Ops
+		Connected,
+		ApproveAccount,
+		ApproveLogin,
+		SendAccountDetails,
+		ApprovePasswordUpdate,
+		AccountDeleted,
+		EnterGameLobby,
+		AddPlayer,
+		AddSpectator,
+		StartGame,
+		CloseConnection
+	}
+}
