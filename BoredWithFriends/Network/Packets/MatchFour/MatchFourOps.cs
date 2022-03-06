35dabd66ab9d06d96a823449d426a3a07a4b2a31
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-05 18:15:34 -0800

    Added Protocol & Opcode Enumerations
    These are for packet implementations so they can specify their protocol and opcode in a way that prevents accidental overlap.

---
+++ BoredWithFriends/Network/Packets/MatchFour/MatchFourOps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Network.Packets.MatchFour
+{
+	internal enum MatchFourOps : short
+	{
+		//Client Ops
+		RequestBoardState,
+		PlayToken,
+		ClearBoard,
+
+		//Server Ops
+		SendBoardState,
+		TokenPlayed,
+		BoardCleared,
+	}
+}
