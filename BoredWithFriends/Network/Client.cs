ac750812a3185a7bd1e5417655eec3a80d6b8fe6
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-09 07:42:34 -0800

    Added Client Class

---
+++ BoredWithFriends/Network/Client.cs
@@ -0,0 +1,29 @@
+﻿using BoredWithFriends.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Network
+{
+	internal static class Client
+	{
+		private static ClientNetworkHandler clientNetworkHandler = null!;
+
+		public static Player ClientPlayer { get; set; } = null!; //Remember to set from login packet
+
+		public static GameState? LocalGameState { get; set; }
+
+		public static void StartClient(string serverIP = "127.0.0.1", int port = 7777)
+		{
+			if (clientNetworkHandler is null)
+			{
+				clientNetworkHandler = new();
+				PacketSendUtility.NetHandler = clientNetworkHandler;
+			}
+
+			clientNetworkHandler.ConnectToServer(serverIP, port);
+		}
+	}
+}
