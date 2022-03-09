6644a5626c8c4e0b8c675a3e6cc6b9e7ba40371f
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-09 08:22:55 -0800

    Corrected Base Class on Server Packets From Last Commit
    I guess that's what I get for copy/pasting code.

--- BoredWithFriends/Network/Packets/General/Server/ServerEnterGameLobby.cs
+++ BoredWithFriends/Network/Packets/General/Server/ServerEnterGameLobby.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace BoredWithFriends.Network.Packets.General.Server
 {
 	[Packet(typeof(ServerEnterGameLobby), BoredWithFriendsProtocol.General, (short) GeneralOps.ServerEnterGameLobby)]
-	internal class ServerEnterGameLobby : ClientPacket
+	internal class ServerEnterGameLobby : ServerPacket
 	{
 		protected override void ReadImpl()
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override void RunImpl(Connection con)
+		protected override void RunImpl()
 		{
 			throw new NotImplementedException();
 		}
--- BoredWithFriends/Network/Packets/General/Server/ServerSendAccountDetails.cs
+++ BoredWithFriends/Network/Packets/General/Server/ServerSendAccountDetails.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace BoredWithFriends.Network.Packets.General.Server
 {
 	[Packet(typeof(ServerSendAccountDetails), BoredWithFriendsProtocol.General, (short) GeneralOps.ServerSendAccountDetails)]
-	internal class ServerSendAccountDetails : ClientPacket
+	internal class ServerSendAccountDetails : ServerPacket
 	{
 		protected override void ReadImpl()
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override void RunImpl(Connection con)
+		protected override void RunImpl()
 		{
 			throw new NotImplementedException();
 		}
