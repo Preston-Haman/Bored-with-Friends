785593150a821abc24c8c08630ba2bbe70a8abe0
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-09 06:48:52 -0800

    Created PacketSendUtility Class
    This class is meant for helper methods to send packets to and from the server. Slightly refactored Server class to support it.

--- BoredWithFriends/Network/PacketSendUtility.cs
+++ BoredWithFriends/Network/PacketSendUtility.cs
@@ -8,22 +8,11 @@ using System.Threading.Tasks;
 
 namespace BoredWithFriends.Network
 {
-	/// <summary>
-	/// A simple utility class for sending outbound packets.
-	/// </summary>
 	internal static class PacketSendUtility
 	{
-		/// <summary>
-		/// The <see cref="NetworkHandler"/> that packets will be transmitted on.
-		/// </summary>
+
 		public static NetworkHandler NetHandler { get; set; } = null!;
 
-		/// <summary>
-		/// Attempts to send the given <paramref name="packet"/> to a remote server
-		/// in charge of the client version of this application.
-		/// </summary>
-		/// <param name="packet">The packet to send.</param>
-		/// <exception cref="InvalidOperationException">If this application is not running as a client.</exception>
 		public static void SendPacket(ClientPacket packet)
 		{
 			if (NetHandler is LocalNetworkHandler)
@@ -41,12 +30,6 @@ namespace BoredWithFriends.Network
 			throw new InvalidOperationException("Cannot send packets to a server while operating as a server.");
 		}
 
-		/// <summary>
-		/// Attempts to send the given <paramref name="packet"/> to the specified <paramref name="player"/>.
-		/// </summary>
-		/// <param name="player">The player to send the <paramref name="packet"/> to.</param>
-		/// <param name="packet">The packet to send to the <paramref name="player"/>.</param>
-		/// <exception cref="InvalidOperationException">If this application is not running as a server.</exception>
 		public static void SendPacket(Player player, ServerPacket packet)
 		{
 			if (NetHandler is LocalNetworkHandler)
@@ -64,13 +47,6 @@ namespace BoredWithFriends.Network
 			throw new InvalidOperationException("Cannot send packets to a client while operating as a client.");
 		}
 
-		/// <summary>
-		/// Attempts to send the given <paramref name="packet"/> to all players associated
-		/// with the game that the given <paramref name="player"/> is taking part in.
-		/// </summary>
-		/// <param name="player">The player taking part in the game of interest.</param>
-		/// <param name="packet">The packet to send to all players of the game <paramref name="player"/> is in.</param>
-		/// <exception cref="InvalidOperationException"></exception>
 		public static void BroadcastPacket(Player player, ServerPacket packet)
 		{
 			if (NetHandler is LocalNetworkHandler)
@@ -101,11 +77,6 @@ namespace BoredWithFriends.Network
 			throw new InvalidOperationException("Cannot send packets to a client while operating as a client.");
 		}
 
-		/// <summary>
-		/// Uses a <see cref="LocalConnection"/> to "send" the packet out to the <see cref="NetHandler"/>;
-		/// this will result in problems if the <see cref="NetHandler"/> is not a <see cref="LocalNetworkHandler"/>.
-		/// </summary>
-		/// <param name="packet">The packet to send.</param>
 		private static void SendLocalPacket(BasePacket packet)
 		{
 			NetHandler.SendPacket(Server.GetLocalConnection(Server.LocalGameState!.getCurrentPlayer()), packet);
