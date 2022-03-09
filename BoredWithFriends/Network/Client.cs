c02b162531d39e29fa63466fa17e11b72f594ece
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-09 07:42:34 -0800

    Added Client Class

--- BoredWithFriends/Network/Client.cs
+++ BoredWithFriends/Network/Client.cs
@@ -7,45 +7,14 @@ using System.Threading.Tasks;
 
 namespace BoredWithFriends.Network
 {
-	/// <summary>
-	/// Holds state for when this application is running as a remote client.
-	/// </summary>
 	internal static class Client
 	{
-		/// <summary>
-		/// The network handler responsible for sending/receiving packet information on the network.
-		/// </summary>
 		private static ClientNetworkHandler clientNetworkHandler = null!;
 
-		/// <summary>
-		/// The current player that is logged in for this remote client.
-		/// </summary>
 		public static Player ClientPlayer { get; set; } = null!; //Remember to set from login packet
 
-		/// <summary>
-		/// The local game state for this client; it should be in an identical state
-		/// to the server side game state.
-		/// </summary>
-		public static GameState? LocalGameState
-		{
-			//Our local state represents the state on the server side
-			//The code is written like this for the sake of local gameplay
-			get
-			{
-				return Server.LocalGameState;
-			}
+		public static GameState? LocalGameState { get; set; }
 
-			set
-			{
-				Server.LocalGameState = value;
-			}
-		}
-
-		/// <summary>
-		/// The entry point for running this application as a remote client.
-		/// </summary>
-		/// <param name="serverIP">The IP address of the remote server.</param>
-		/// <param name="port">The port of the remote server to connect to.</param>
 		public static void StartClient(string serverIP = "127.0.0.1", int port = 7777)
 		{
 			if (clientNetworkHandler is null)
