fd4df08877ba6990e158c61eca093675c36c71a0
Kris <dauthi66@gmail.com>
2022-03-09 19:26:55 -0800

    Refactored GetPlayerStatistics. Removed unnecessary NameExists method. Added Documentation to DeleteUser

--- BoredWithFriends/Data/DatabaseUtility.cs
+++ BoredWithFriends/Data/DatabaseUtility.cs
@@ -79,25 +79,6 @@ namespace BoredWithFriends.Data
 		}
 
 		/// <summary>
-		/// Checks if a user name exists in the database
-		/// </summary>
-		/// <param name="userName">The user name to search for</param>
-		/// <returns>True if found</returns>
-		public static bool NameExists(string userName)
-		{
-			DatabaseContext database = new();
-			PlayerLogin? user = (from logins in database.PlayerLogins
-								 where logins.UserName == userName
-								 select logins).SingleOrDefault();
-
-			if (user is null)
-			{
-				return false;
-			}
-			return true;
-		}
-
-		/// <summary>
 		/// Updates a user's password by searching for 
 		/// their username in the database
 		/// </summary>
@@ -117,7 +98,7 @@ namespace BoredWithFriends.Data
 		/// <summary>
 		/// Deletes all data in database under given userName
 		/// </summary>
-		/// <param name="userName"></param>
+		/// <param name="userName">The user name of the data to delete</param>
 		public static void DeleteUser(string userName)
 		{
 			PlayerLogin user = GetPlayerLogin(userName);
