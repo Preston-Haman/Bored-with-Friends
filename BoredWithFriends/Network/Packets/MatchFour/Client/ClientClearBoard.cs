b9db5868a1d5090271aa8e37e39c8f5e3171ad86
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-10 13:27:33 -0800

    Partially Implemented MatchFour Packets

--- BoredWithFriends/Network/Packets/MatchFour/Client/ClientClearBoard.cs
+++ BoredWithFriends/Network/Packets/MatchFour/Client/ClientClearBoard.cs
@@ -6,23 +6,34 @@ using System.Threading.Tasks;
 
 namespace BoredWithFriends.Network.Packets.MatchFour.Client
 {
+	/// <summary>
+	/// Sent by the client when a user clears the board.
+	/// </summary>
 	[Packet(typeof(ClientClearBoard), BoredWithFriendsProtocol.MatchFour, (short) MatchFourOps.ClientClearBoard)]
 	internal class ClientClearBoard : ClientPacket
 	{
+		private bool playerForfeited;
+
+		public ClientClearBoard(bool playerForfeited)
+		{
+			this.playerForfeited = playerForfeited;
+		}
+
 		protected override void ReadImpl()
 		{
-			//Nothing to do.
+			playerForfeited = ReadBool();
 		}
 
 		protected override void RunImpl(Connection con)
 		{
 			//TODO: Get gamestate for this packet and forfeit this player; reply with BoardCleared
+			//If the player is not forfeiting, make sure that the game is over!
 			throw new NotImplementedException();
 		}
 
 		protected override void WriteImpl()
 		{
-			//Nothing to do.
+			WriteBool(playerForfeited);
 		}
 	}
 }
