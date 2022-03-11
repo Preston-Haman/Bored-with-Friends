53770e89d0aba9d0e6c43c9ed7136e1dfbeef26e
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-11 14:54:28 -0800

    Added Events to Client Class

--- BoredWithFriends/Network/Client.cs
+++ BoredWithFriends/Network/Client.cs
@@ -1,4 +1,5 @@
 ﻿using BoredWithFriends.Games;
+using BoredWithFriends.Network.Packets.General.Server;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,20 @@ namespace BoredWithFriends.Network
 		/// The network handler responsible for sending/receiving packet information on the network.
 		/// </summary>
 		private static ClientNetworkHandler clientNetworkHandler = null!;
+
+		/// <summary>
+		/// An event that is raised when the client has connected to the server and is ready
+		/// to send credentials.
+		/// <br></br><br></br>
+		/// Subscribers are responsible for calling <see cref="PacketSendUtility.SendPacket(Packets.ClientPacket)"/>
+		/// with an instance of <see cref="Packets.General.Client.ClientLogin"/>.
+		/// </summary>
+		public static event EventHandler? LoginReadyEvent;
+
+		/// <summary>
+		/// An event that is raised when the client connection to the server has been terminated.
+		/// </summary>
+		public static event EventHandler? ClientStoppedEvent;
 
 		/// <summary>
 		/// The <see cref="Player"/> value for <see cref="ClientPlayer"/>.
@@ -38,7 +53,10 @@ namespace BoredWithFriends.Network
 
 			set
 			{
-				clientPlayer = value;
+				if (clientNetworkHandler is not null)
+				{
+					clientPlayer = value;
+				}
 			}
 		}
 
@@ -77,12 +95,47 @@ namespace BoredWithFriends.Network
 			clientNetworkHandler.ConnectToServer(serverIP, port);
 		}
 
+		/// <summary>
+		/// Raises the <see cref="LoginReadyEvent"/> so subscribers may send the
+		/// login packet with credentials.
+		/// </summary>
+		/// <param name="serverConnectedPacket"></param>
+		public static void RaiseLoginReadyEvent(ServerConnected serverConnectedPacket)
+		{
+			EventHandler? loginReady = LoginReadyEvent;
+
+			if (loginReady is not null)
+			{
+				loginReady(serverConnectedPacket, EventArgs.Empty);
+			}
+		}
+
+		/// <summary>
+		/// Raises the <see cref="ClientStoppedEvent"/> so subscribers may
+		/// do something about it.
+		/// </summary>
+		/// <param name="sender">The optional object that is raising this event.</param>
+		public static void RaiseClientStoppedEvent(object? sender = null)
+		{
+			EventHandler? clientStopped = ClientStoppedEvent;
+
+			if (clientStopped is not null)
+			{
+				clientStopped(sender, EventArgs.Empty);
+			}
+		}
+
+		/// <summary>
+		/// Stops the client network handling, and raises the <see cref="ClientStoppedEvent"/> so subscribers know.
+		/// </summary>
 		public static void StopClient()
 		{
 			if (clientNetworkHandler is not null)
 			{
 				clientNetworkHandler.Stop();
 			}
+
+			RaiseClientStoppedEvent();
 		}
 	}
 }
