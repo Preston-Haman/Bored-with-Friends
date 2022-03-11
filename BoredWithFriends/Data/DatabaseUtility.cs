d632772fbf9b11ae7a2507cc3c8a803c1cee3726
Kris <dauthi66@gmail.com>
2022-03-10 17:15:06 -0800

    Changed methods to use PlayerLogin objects to avoid unneeded database checks. Undid NameExists removal :p

--- BoredWithFriends/Data/DatabaseUtility.cs
+++ BoredWithFriends/Data/DatabaseUtility.cs
@@ -60,13 +60,11 @@ namespace BoredWithFriends.Data
 		/// <param name="userName">The userName to search for</param>
 		/// <returns>The PlayerStatistics Object related to the userName from PlayerLogin table</returns>
 		/// <exception cref="ArgumentNullException">If a PlayerStatistics PlayerID is not found in the table</exception>
-		public static PlayerStatistics GetPlayerStatistics(string userName)
+		public static PlayerStatistics GetPlayerStatistics(PlayerLogin user)
 		{
-			int playerID = GetPlayerLogin(userName).PlayerID;
-
 			DatabaseContext database = new();
 
-			PlayerStatistics? userStats = database.PlayerStatistics.Find(playerID);
+			PlayerStatistics? userStats = database.PlayerStatistics.Find(user.PlayerID);
 
 			if (userStats != null)
 			{
@@ -74,20 +72,38 @@ namespace BoredWithFriends.Data
 			}
 			else
 			{
-				throw new ArgumentNullException($"No PlayerStatistics PlayerID under {userName}");
+				throw new ArgumentNullException($"No PlayerStatistics PlayerID under {user.UserName}");
 			}
 		}
 
-		/// <summary>
-		/// Updates a user's password by searching for 
-		/// their username in the database
-		/// </summary>
-		/// <param name="userName">The user to change passwords</param>
-		/// <param name="password">The password to change to</param>
-		public static void UpdatePassword(string userName, string password)
-		{
-			PlayerLogin user = GetPlayerLogin(userName);
 
+		/// <summary>
+		/// Checks if a user name exists in the database
+		/// </summary>
+		/// <param name="userName">The user name to search for</param>
+		/// <returns>True if found</returns>
+		public static bool NameExists(string userName)
+		{
+			DatabaseContext database = new();
+			PlayerLogin? user = (from logins in database.PlayerLogins
+								 where logins.UserName == userName
+								 select logins).SingleOrDefault();
+
+			if (user is null)
+			{
+				return false;
+			}
+			return true;
+		}
+
+
+		/// <summary>
+		/// Updates a user's password by using their PlayerLogin and given string
+		/// </summary>
+		/// <param name="user">The user to change passwords</param>
+		/// <param name="password">The password to change to</param>
+		public static void UpdatePassword(PlayerLogin user, string password)
+		{
 			user.Password = password;
 
 			DatabaseContext database = new();
@@ -96,17 +112,71 @@ namespace BoredWithFriends.Data
 		}
 
 		/// <summary>
-		/// Deletes all data in database under given userName
+		/// Deletes all data in database linked to a PlayerLogin
 		/// </summary>
 		/// <param name="userName">The user name of the data to delete</param>
-		public static void DeleteUser(string userName)
+		public static void DeleteUser(PlayerLogin user)
 		{
-			PlayerLogin user = GetPlayerLogin(userName);
-			PlayerStatistics userStatistics = GetPlayerStatistics(userName);
+			PlayerStatistics userStatistics = GetPlayerStatistics(user);
+			
+			DatabaseContext database = new();			
 
-			DatabaseContext database = new();
 			database.Remove(user);
 			database.Remove(userStatistics);
+			database.SaveChangesAsync();
+		}
+		/// <summary>
+		/// Adds a win when a player wins a game
+		/// </summary>
+		/// <param name="user">User to add a win to</param>
+		public static void AddWin(PlayerLogin user)
+		{
+			PlayerStatistics userStats = GetPlayerStatistics(user);
+			userStats.Wins++;
+			userStats.RoundsPlayed++;
+
+			DatabaseContext database = new();
+			database.Update(userStats);
+			database.SaveChangesAsync();
+		}
+		/// <summary>
+		/// Adds a loss when a player loses a game 
+		/// </summary>
+		/// <param name="user">User to give one loss to</param>
+		public static void AddLoss(PlayerLogin user)
+		{			
+			PlayerStatistics userStats = GetPlayerStatistics(user);
+			userStats.Losses++;
+			userStats.RoundsPlayed++;
+
+			DatabaseContext database = new();
+			database.Update(userStats);
+			database.SaveChangesAsync();
+			
+		}
+
+		/// <summary>
+		/// Adds up total time logged in during a session to reflect
+		/// how long a user has been logged in since creation.
+		/// </summary>
+		/// <exception cref="NotImplementedException"></exception>
+		public static void AddPlayTime()
+		{
+			//TODO: might be easier to add when we implement the login function
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Changes LastPlayTime to reflect the last time the user logged in
+		/// </summary>
+		/// <param name="user">user to change LastPlayTime</param>
+		public static void UpdateLastPlayTime(PlayerLogin user)
+		{
+			PlayerStatistics userStats = GetPlayerStatistics(user);
+			userStats.LastPlayedTime = DateTime.Now;
+
+			DatabaseContext database = new();
+			database.Update(userStats);
 			database.SaveChangesAsync();
 		}
 	}
