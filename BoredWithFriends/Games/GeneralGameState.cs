eddfee613e86e6a69642feaa53422f3ef3cd2fa4
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-13 20:12:40 -0700

    Created GeneralGameState Class

---
+++ BoredWithFriends/Games/GeneralGameState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredWithFriends.Games
+{
+	internal class GeneralGameState : GameState
+	{
+		public GeneralGameState(IGeneralGameGui generalGameGui) : base(generalGameGui)
+		{
+			//Do nothing.
+		}
+
+		public void AddPlayer(Player player)
+		{
+			AddCompetingPlayers(player);
+		}
+
+		public override Player GetCurrentPlayer()
+		{
+			return Players[0];
+		}
+
+		public override int GetMaxPlayers()
+		{
+			return short.MaxValue; //Arbitrary.
+		}
+
+		public override void ResetGame()
+		{
+			//Nothing to reset.
+		}
+	}
+}
