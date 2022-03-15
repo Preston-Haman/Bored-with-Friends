2ac23a7a90f8e71a3db708505e98f9d5fdc1a38e
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-03-15 07:26:31 -0700

    Added Database Migrations
    closes #50

---
+++ BoredWithFriends/Migrations/20220315140532_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoredWithFriends.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlayerStatistics",
+                columns: table => new
+                {
+                    PlayerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastPlayedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalPlayTime = table.Column<long>(type: "bigint", nullable: false),
+                    RoundsPlayed = table.Column<int>(type: "int", nullable: false),
+                    Wins = table.Column<int>(type: "int", nullable: false),
+                    Losses = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerStatistics", x => x.PlayerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlayerLogins",
+                columns: table => new
+                {
+                    PlayerID = table.Column<int>(type: "int", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastLoginTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerLogins", x => x.PlayerID);
+                    table.ForeignKey(
+                        name: "FK_PlayerLogins_PlayerStatistics_PlayerID",
+                        column: x => x.PlayerID,
+                        principalTable: "PlayerStatistics",
+                        principalColumn: "PlayerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerLogins_UserName",
+                table: "PlayerLogins",
+                column: "UserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlayerLogins");
+
+            migrationBuilder.DropTable(
+                name: "PlayerStatistics");
+        }
+    }
+}
